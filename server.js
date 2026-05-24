1c967ada14b4c072d6b30c4702f4305e02081f81
FrancisNdirangu <frankngugi@gmail.com>
2026-05-24 23:16:14 +0300

    successfully implemented a get request for searching for movies on the tmdb api

--- server.js
+++ server.js
@@ -10,13 +10,31 @@ const bearer_token = process.env.API_KEY;
 const config = {
   headers: {
     accept: 'application/json',
-    Authorization : ` Bearer ${bearer_token}`
+    Authorization : `Bearer ${bearer_token}`
   }
 };
 
 const app = express();
 const port = 3000;
 
+app.get("/search", async (req,res) => {
+  try {
+    const request = await axios.get(search_url,
+      {params: {
+        query:'The Matrix',
+        include_adult: false,
+        language: 'en-US'
+      }, 
+    headers: {
+      accept: 'application/json',
+      Authorization : `Bearer ${bearer_token}`
+    }});
+    res.json(request.data);
+    console.log(request);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Running on port: ${port}`);
