b2affb69aaacd2426f8ebe2d85c0ccb35101f33d
FrancisNdirangu <frankngugi@gmail.com>
2026-05-26 00:04:39 +0300

    printing the request.data to the terminal so i can analyse the paths for the images ratings and etx

--- server.js
+++ server.js
@@ -9,41 +9,42 @@ const search_url = "https://api.themoviedb.org/3/search/movie";
 const bearer_token = process.env.API_KEY;
 const config = {
   headers: {
-    accept: 'application/json',
-    Authorization : `Bearer ${bearer_token}`
-  }
+    accept: "application/json",
+    Authorization: `Bearer ${bearer_token}`,
+  },
 };
 
 const app = express();
 const port = 3000;
 
 // setting the static folder in express
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // 1. set the engine which is ejs in this case
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // 2. set the folder where your views live
-app.set('views','./public');
+app.set("views", "./public");
 
-app.get("/", (req,res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get("/search", async (req,res) => {
+app.get("/search", async (req, res) => {
   try {
-    const request = await axios.get(search_url,
-      {params: {
-        query:'The Matrix',
+    const request = await axios.get(search_url, {
+      params: {
+        query: "The Matrix",
         include_adult: false,
-        language: 'en-US'
-      }, 
-    headers: {
-      accept: 'application/json',
-      Authorization : `Bearer ${bearer_token}`
-    }});
+        language: "en-US",
+      },
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${bearer_token}`,
+      },
+    });
     res.json(request.data);
-    console.log(request);
+    console.log(request.data);
   } catch (error) {
     console.error(error);
   }
