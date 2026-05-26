cf355431d5378c43ef860552f535cef8c50f4dc2
FrancisNdirangu <frankngugi@gmail.com>
2026-05-26 23:27:38 +0300

    commented out code that im no longer using

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ import axios from "axios";
 
 const search_url = "https://api.themoviedb.org/3/search/movie";
 const bearer_token = process.env.API_KEY;
-const config = {
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${bearer_token}`,
-  },
-};
+// commented out the config below since im passing it directly in each axios request below
+// const config = {
+//   headers: {
+//     accept: "application/json",
+//     Authorization: `Bearer ${bearer_token}`,
+//   },
+// };
 
 const app = express();
 const port = 3000;
@@ -70,25 +71,26 @@ app.post('/submit', async(req, res) => {
   res.render('results');
 });
 
-app.get("/search", async (req, res) => {
-  try {
-    const request = await axios.get(search_url, {
-      params: {
-        query: "The Matrix",
-        include_adult: false,
-        language: "en-US",
-      },
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${bearer_token}`,
-      },
-    });
-    res.json(request.data);
-    console.log(request.data);
-  } catch (error) {
-    console.error(error);
-  }
-});
+// commented out this get request since we are now using it from inside the post request above
+// app.get("/search", async (req, res) => {
+//   try {
+//     const request = await axios.get(search_url, {
+//       params: {
+//         query: "The Matrix",
+//         include_adult: false,
+//         language: "en-US",
+//       },
+//       headers: {
+//         accept: "application/json",
+//         Authorization: `Bearer ${bearer_token}`,
+//       },
+//     });
+//     res.json(request.data);
+//     console.log(request.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Running on port: ${port}`);
